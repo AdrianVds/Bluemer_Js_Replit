dd1457a76f32bb6fa772fd109f1ebd26fbb902c1
Adrian Vinicius <85651273+AdrianVds@users.noreply.github.com>
2021-08-18 15:41:16 +0000

    Initial commit

---
+++ aula01.js
@@ -0,0 +1,7 @@
+//console.log('ola mundo!');
+//prompt('Digite seu nome: ');
+// let usuario = 'Adrian vinicius';
+// console.log(usuario);
+let aluno = prompt("Digite seu nome: ");
+let turma = prompt("Digite sua turma: ");
+console.log('Boa noite ' + aluno + ', seja bem vindo á turma ' + turma);
---
+++ aula02.js
@@ -0,0 +1,19 @@
+let vartexto = "adrian";
+let varnumero = 3;
+console.log(vartexto);
+console.log(varnumero);
+let numero1 = 50;
+let numero2 = 60;
+let comparação = numero1 > numero2;
+console.log(comparação);
+let array = ["adrian","miguel","professor","mateus",["array2","array3"]];
+let arraynumber = [10,20,30,40,50];
+let arraymisto = ["jose",true,50];
+console.log(array[4]);
+console.log(arraynumber[2]);
+console.log(arraymisto[2]);
+typeof array;
+var numero = prompt("digite um numero");
+var numero2 = prompt("digite outro numero");
+var soma = numero + numero2;
+console.log(soma)
